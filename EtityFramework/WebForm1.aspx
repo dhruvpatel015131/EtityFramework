3a0b29a7fbaaff8f105ee9b3a153fca9dbaecc0c
dhruvpatel015131 <140480404+dhruvpatel015131@users.noreply.github.com>
2024-06-15 06:55:45 +0530

    Add project files.

---
+++ EtityFramework/WebForm1.aspx
@@ -0,0 +1,66 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="EtityFramework.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    
+    <form id="form1" runat="server">
+        <div>
+            <center>
+             <asp:Label ID="Label1" runat="server" Text="Entity Framework"></asp:Label>
+            <br />
+            
+            </center>
+            <asp:Label ID="Label2" runat="server" Text="Enter Roll Number :"></asp:Label>
+            &nbsp;&nbsp;
+            <asp:TextBox ID="txtRollno" runat="server"></asp:TextBox>
+            &nbsp;<br />
+
+            <asp:Label ID="Label3" runat="server" Text="Enter Name :"></asp:Label>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtName" runat="server"></asp:TextBox>
+            <br />
+            <asp:Label ID="Label4" runat="server" Text="Enter Address"></asp:Label>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:TextBox ID="txtAddress" runat="server"></asp:TextBox>
+             <br />
+            <br />
+             <asp:GridView ID="GridView1" runat="server" AutoGenerateSelectButton="True" PageSize="2" AllowPaging="True">
+             </asp:GridView>
+            <br />
+            <br />
+             &nbsp;&nbsp;
+           
+            <asp:Button ID="btnNext" runat="server" OnClick="btnNext_Click" Text="Next" Width="54px" />
+            <asp:Button ID="btnPrevious" runat="server" OnClick="btnPrevious_Click" style="margin-left: 44px" Text="Previous" />
+           
+            <br />
+            <br />
+            <asp:Button ID="btnAdd" runat="server" Text="Add" OnClick="btnAdd_Click" />
+&nbsp;
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+&nbsp;
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+&nbsp;
+            <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click"  />
+&nbsp;
+            
+              <br />
+            <asp:Label ID="Label5" runat="server" Text="Label"></asp:Label>
+            <br />
+
+
+
+
+
+
+
+        </div>
+    </form>
+
+</body>
+</html>
---
+++ EtityFramework/WebForm1.aspx.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EtityFramework
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        studDBEntities dc = new studDBEntities();
+
+
+        public void showData()
+        {
+            try
+            {
+                GridView1.DataSource = dc.studs.ToList();
+                GridView1.DataBind();
+            }
+            catch (Exception ex)
+            {
+                Label5.Text = "Error Occured! " + ex.Message;
+            }
+            finally { }
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            showData();
+        }
+
+  
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stud addObj = new stud();
+                addObj.rollno = Convert.ToInt32(txtRollno.Text);
+                addObj.name = txtName.Text.ToString();
+                addObj.address = txtAddress.Text .ToString();
+
+                dc.studs.Add(addObj);
+                dc.SaveChanges();
+                showData();
+
+              
+            }
+            catch (Exception ex)
+            {
+                Label5.Text = "Error Occured! " + ex.Message;
+            }
+            finally { }
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stud delObj = dc.studs.Find(Convert.ToInt32(txtRollno.Text));
+
+                if (delObj != null)
+                {
+                    dc.studs.Remove(delObj);
+                    dc.SaveChanges();
+                    Label5.Text = "Deleted Succesfully";
+                    showData();
+                }
+             
+            }
+            catch (Exception ex)
+            {
+                Label5.Text = "Error Occured! " + ex.Message;
+            }
+            finally { }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stud updateObj = dc.studs.Find(Convert.ToInt32(txtRollno.Text));
+                if (updateObj!=null)
+                {
+                    updateObj.name = txtName.Text.ToString();
+                    updateObj.address = txtAddress.Text.ToString();
+                    dc.SaveChanges();
+                    showData();
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Label5.Text = "Error Occured! " + ex.Message;
+            }
+            finally { }
+        }
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                stud serObj = dc.studs.Find(Convert.ToInt32(txtRollno.Text));
+                if (serObj != null)
+                {
+                    txtName.Text = serObj.name;
+                    txtAddress.Text= serObj.address;
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Label5.Text = "Error Occured! " + ex.Message;
+            }
+            finally { }
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            txtRollno.Text = GridView1.SelectedRow.Cells[1].Text;
+            txtName.Text = GridView1.SelectedRow.Cells[2].Text;
+            txtAddress.Text = GridView1.SelectedRow.Cells[3].Text;
+
+        }
+
+
+        protected void btnNext_Click(object sender, EventArgs e)
+        {
+          
+            if(GridView1.PageIndex < GridView1.PageCount)
+            {
+                GridView1.PageIndex = GridView1.PageIndex + 1;
+                GridView1.DataBind();
+            }
+            
+        }
+
+        protected void btnPrevious_Click(object sender, EventArgs e)
+        {
+            GridView1.PageIndex = GridView1.PageIndex - 1;
+            if(GridView1.PageIndex >0)
+            {
+                GridView1.PageIndex = 1;
+            }
+            GridView1.DataBind();
+        }
+    }
+}
